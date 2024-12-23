f8c2e88a88d701ee6b1f57c315ce2b3076e8079b
MatthSy <matthieu.symphorien@edu.ece.fr>
2024-12-23 18:03:53 +0100

    Added a request handler for "/passwords"

--- src/routes/get.rs
+++ src/routes/get.rs
@@ -1,16 +1,35 @@
+use std::fmt::format;
 use rocket::get;
 use rocket::sentinel::resolution::Resolve;
 use std::fs::File;
 use std::io::Read;
 
 #[get("/")]
-pub(crate) async fn index() {
-    println!("GET request on index");
+pub(crate) async fn index() {}
+
+#[get("/passwords")]
+pub(crate) async fn all_passwords_id() -> Result<Vec<String>, GetPassError> {
+    let files = std::fs::read_dir("data");
+    if files.is_err() {
+        return Err(GetPassError {
+            message: "Error reading data directory".to_string(),
+        });
+    }
+    let files = files.unwrap();
+    let mut res = Vec::new();
+    for file in files {
+        let file = file.unwrap();
+        let file_name = file.file_name();
+        let file_name = file_name.to_str().unwrap();
+        res.push(file_name.to_string());
+    }
+    Ok(res)
+
 }
 
-#[get("/password/<password_id>")]
+#[get("/passwords/<password_id>")]
 pub(crate) async fn password(password_id: &str) -> Result<Vec<u8>, GetPassError> {
-    let file = File::open("data/".to_owned() + password_id);
+    let file = File::open(format!("data/{}", password_id));
     if file.is_err() {
         return Err(GetPassError {
             message: "Password not found or other internal error".to_string(),
